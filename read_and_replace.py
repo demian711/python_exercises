c51ab60164d67fff713ea004b58c51f73496dd98
Demian Sciessere <Demian711@demian-2.local>
2016-04-05 00:07:21 -0300

    improvement for read and replace, now it takes a bunch of files and changes them all

--- read_and_replace.py
+++ read_and_replace.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
-
-
-file_to_change = "RMS-res_2FBW_A-2H88_N.txt"
+import glob
 
 def read_file(file, per):
     with open(file, per) as file :
-        return file.read()     
+        return file.read()   
 
 def write_file(file, per, content):
     with open(file, per) as file :
@@ -16,7 +13,13 @@ def replace_all(content, dic):
         content = content.replace(i, j)
     return content
 
-reps = {'   A':'    ', '   N':'    '}
-file_content = read_file(file_to_change, 'r')
-pre_modified = replace_all(file_content, reps)
-write_file(file_to_change, 'w', pre_modified)
+
+for arch in glob.glob("RMS*"):
+    first = arch[13]
+    second = arch[20]
+    reps = {'   '+first:'    ', '   '+second:'    '}
+    file_content = read_file(arch, 'r')
+    pre_modified = replace_all(file_content, reps)
+    write_file(arch, 'w', pre_modified)
+
+print "ok McKay"
